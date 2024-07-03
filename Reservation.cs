32d4a120bf35a5c5c0005af3226604b579c81a30
annammaxavier <166633700+annammaxavier@users.noreply.github.com>
2024-07-02 22:41:30 -0600

    Add files via upload

---
+++ Reservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightApp.Components.Data
+{
+    public abstract class Reservation
+    {
+        public string? PassengerName { get; protected set; }
+        public string? Citizenship { get; protected set; }
+        public Flight? ReservedFlight { get; protected set; }
+
+        // Abstract method for confirming the reservation
+        public abstract void ConfirmReservation();
+
+        internal void CancelReservation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
