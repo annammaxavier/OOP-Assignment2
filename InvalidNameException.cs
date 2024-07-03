033f19f8c554ad5ec24f23cf87bbd9b86a834751
annammaxavier <166633700+annammaxavier@users.noreply.github.com>
2024-07-02 22:40:51 -0600

    Add files via upload

---
+++ InvalidNameException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightApp.Components.Data
+{
+    public class InvalidNameException
+    {
+        public InvalidNameException() : this("Invalid name provided.") { }
+
+        public InvalidNameException(string v)
+        {
+            V = v;
+        }
+
+        public string V { get; }
+    }
+}
