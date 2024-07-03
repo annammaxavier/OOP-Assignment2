bf7b40e0d1995462ace16744e8691836eb96282d
annammaxavier <166633700+annammaxavier@users.noreply.github.com>
2024-07-02 22:42:21 -0600

    Add files via upload

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightApp.Components.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public required string Summary { get; set; }
+    }
+}
