6003db3abbee18553cea950ae827ba68011d0692
annammaxavier <166633700+annammaxavier@users.noreply.github.com>
2024-07-02 22:40:18 -0600

    Add files via upload

---
+++ InvalidCitizenshipException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightApp.Components.Data
+{
+    public class InvalidCitizenshipException
+    {
+        public InvalidCitizenshipException() : this("Invalid citizenship information provided.") { }
+
+        public InvalidCitizenshipException(string v)
+        {
+        }
+    }
+}
+
