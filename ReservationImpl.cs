5730cdccca0af8eac67c74a94042a6ac2c443e82
annammaxavier <166633700+annammaxavier@users.noreply.github.com>
2024-07-02 22:41:45 -0600

    Add files via upload

---
+++ ReservationImpl.cs
@@ -0,0 +1,19 @@
+﻿namespace FlightApp.Components.Data
+{
+    internal class ReservationImpl : Reservation
+    {
+        private Flight flight;
+
+        public ReservationImpl(string passengerName, string citizenship, Flight flight)
+        {
+            PassengerName = passengerName;
+            Citizenship = citizenship;
+            this.flight = flight;
+        }
+
+        public override void ConfirmReservation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
