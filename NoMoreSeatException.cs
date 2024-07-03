bdb3d32b72195324ed7e1e36b2055180aef9f62e
annammaxavier <166633700+annammaxavier@users.noreply.github.com>
2024-07-02 22:41:15 -0600

    Add files via upload

---
+++ NoMoreSeatException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace FlightApp.Components.Data
+{
+    [Serializable]
+    internal class NoMoreSeatException : Exception
+    {
+        public NoMoreSeatException()
+        {
+        }
+
+        public NoMoreSeatException(string? message) : base(message)
+        {
+        }
+
+        public NoMoreSeatException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected NoMoreSeatException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
